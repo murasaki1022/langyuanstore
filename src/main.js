27a940d75b3f9aeb28875a197da4f6e65163f07b
murasaki1022 <murasaki1022@gmail.com>
2024-02-17 01:49:44 +0800

    環境建立

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import 'bootstrap/scss/bootstrap.scss'
+// 引入 VeeValidate 元件跟功能
+import {
+  Field, Form, ErrorMessage, defineRule, configure
+} from 'vee-validate'
+// 引入 VeeValidate 的驗證規則
+import * as AllRules from '@vee-validate/rules'
+// 引入 VeeValidate 的 i18n 功能
+import { localize, setLocale } from '@vee-validate/i18n'
+// 引入 VeeValidate 的繁體中文語系檔
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+
+import App from './App.vue'
+import router from './router'
+
+// 使用 Object.keys 將 AllRules 轉為陣列並使用 forEach 迴圈將驗證規則加入 VeeValidate
+Object.keys(AllRules).forEach((rule) => {
+  defineRule(rule, AllRules[rule])
+})
+
+// 將當前 VeeValidate 的語系設定為繁體中文
+configure({
+  generateMessage: localize({ zh_TW: zhTW }),
+  validateOnInput: true
+})
+setLocale('zh_TW')
+
+const app = createApp(App)
+// 掛載 Global 的 VeeValidate 元件
+app.component('VField', Field)
+app.component('VForm', Form)
+app.component('ErrorMessage', ErrorMessage)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')

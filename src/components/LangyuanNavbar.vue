432b97e03ca257db6add98c84d064b31da5ca94e
tvxq5206 <tvxq5206@gmail.com>
2024-02-24 22:06:53 +0800

    首頁切版

--- src/components/LangyuanNavbar.vue
+++ src/components/LangyuanNavbar.vue
@@ -1,4 +1,5 @@
 <template>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
         <router-link class="navbar-brand" to="/">
@@ -10,17 +11,20 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <router-link class="nav-link" to="/">首頁</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/products">商品列表</router-link>
+              <router-link class="nav-link" to="/">回到首頁</router-link>
             </li>
             <li class="nav-item">
               <router-link class="nav-link" to="/about">關於我們</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/cart">購物車</router-link>
+              <router-link class="nav-link" to="/products">商品專區</router-link>
             </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="">關於我們</router-link>
+            </li>
+            <p class="fs-2 bi bi-search text-primary mx-2"></p>
+            <p class="fs-2 bi bi-clipboard2-check text-primary mx-2"></p>
+            <router-link to="/cart"><p class="fs-2 bi bi-cart4 mx-2"></p></router-link>
           </ul>
         </div>
       </div>
@@ -28,6 +32,7 @@
 </template>
 
 <script>
+
 import * as bootstrap from 'bootstrap'
 window.bootstrap = bootstrap
 export default {
--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -1,12 +1,143 @@
 <template>
 <h1>這是首頁</h1>
+<div class="container">
+    <!--<swiper :pagination="true" :modules="modules" class="mySwiper">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+  </swiper>-->
+  <p class="d-flex justify-content-center fs-2 fw-bold mb-3">熱銷商品</p>
+<div class="d-flex justify-content-center">
+    <div class="row row-cols-md-4 row-cols-sm-2 g-3">
+    <div class="col">
+    <div class="card" style="width: 14rem;">
+     <img src="https://images.unsplash.com/photo-1605533914438-af2b1018b1b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNhdCUyMGZvb2R8ZW58MHx8MHx8fDA%3D" class="card-img-top" alt="">
+      <div class="card-body">
+       <h5 class="card-title">益生菌消臭豆腐砂 原味(米粒型) 7L</h5>
+       <p class="card-text">NT$199</p>
+       <a href="#" class="btn btn-primary d-flex justify-content-center">立即購買</a>
+    </div>
+  </div>
+</div>
+<div class="col">
+    <div class="card" style="width: 14rem;">
+     <img src="https://images.unsplash.com/photo-1605533914438-af2b1018b1b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNhdCUyMGZvb2R8ZW58MHx8MHx8fDA%3D" class="card-img-top" alt="">
+      <div class="card-body">
+       <h5 class="card-title">益生菌消臭豆腐砂 原味(米粒型) 7L</h5>
+       <p class="card-text">NT$199</p>
+       <a href="#" class="btn btn-primary d-flex justify-content-center">立即購買</a>
+    </div>
+  </div>
+</div>
+<div class="col">
+    <div class="card" style="width: 14rem;">
+     <img src="https://images.unsplash.com/photo-1605533914438-af2b1018b1b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNhdCUyMGZvb2R8ZW58MHx8MHx8fDA%3D" class="card-img-top" alt="">
+      <div class="card-body">
+       <h5 class="card-title">益生菌消臭豆腐砂 原味(米粒型) 7L</h5>
+       <p class="card-text">NT$199</p>
+       <a href="#" class="btn btn-primary d-flex justify-content-center">立即購買</a>
+    </div>
+  </div>
+</div>
+<div class="col">
+    <div class="card" style="width: 14rem;">
+     <img src="https://images.unsplash.com/photo-1605533914438-af2b1018b1b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNhdCUyMGZvb2R8ZW58MHx8MHx8fDA%3D" class="card-img-top" alt="">
+      <div class="card-body">
+       <h5 class="card-title">益生菌消臭豆腐砂 原味(米粒型) 7L</h5>
+       <p class="card-text">NT$199</p>
+       <a href="#" class="btn btn-primary d-flex justify-content-center">立即購買</a>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+<div class="gap-2 col-6 mx-auto d-flex justify-content-center mt-5">
+   <div class="btn btn-outline-primary btn-md px-4" type="button">查看更多<br>More</div>
+</div>
+</div>
+<div class="bg-secondary3 pb-5">
+    <div class="d-flex justify-content-center fs-2 fw-bold mt-5 pt-5">好評推薦</div>
+    <div class="row-cols-2 d-flex justify-content-center mt-5">
+    <div class="row">
+    <div class="col">
+      <img src="https://images.unsplash.com/photo-1562248852-58987553bd46?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" style="width: 14rem;" alt="">
+      <p class="fs-4 fw-bold">扭扭</p>
+      <p>挑嘴貓無膠主食罐</p>
+      <p>自從在寵物展買過汪喵挑嘴罐後，扭扭一試成主顧，終於！終結了媽媽找不到適合又安心的罐罐噩夢！</p>
+    </div>
+    <div class="col">
+      <img src="https://images.unsplash.com/photo-1616044543567-a31e09b178ee?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" style="width: 14rem;" alt="">
+      <p class="fs-4 fw-bold">來福</p>
+      <p>95% 鮮肉無膠主食罐</p>
+      <p>來福食量大很難全濕食，但又怕營養不夠，所以偶爾會在來福的飼料裡面拌汪喵罐罐，營養均衡，長得更帥了！</p>
+    </div>
+    <div class="col">
+      <img src="https://images.unsplash.com/photo-1580238169544-86bf7cd8c84c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" style="width: 14rem;" alt="">
+      <p class="fs-4 fw-bold">麻糬</p>
+      <p>冷凍乾燥生食餐</p>
+      <p>為了讓麻糬轉吃生食而先選擇凍乾，結果嗜口性超好，不攝取澱粉飼料後，麻糬體態變得精實、大便比較不臭也不再容易軟便！</p>
+    </div>
+    </div>
+    </div>
+</div>
+<div class="bg-primary mt-5 pt-5 pb-5">
+    <div class="container d-flex justify-content-center">
+    <div class="fw-bold">
+        <div class="row">
+            <div class="col">
+                <ul>關於浪元
+                    <li class="fs-6">品牌故事</li>
+                </ul>
+            </div>
+            <div class="col">
+                <ul>浪圓集團
+                    <li>浪元小賣店</li>
+                    <li>浪圓</li>
+                </ul>
+            </div>
+            <div class="col">
+                <ul>常見問題
+                    <li>FAQ</li>
+                    <li>付款說明</li>
+                    <li>運送說明</li>
+                    <li>退換貨說明</li>
+                    <li>意見回饋</li>
+                </ul>
+            </div>
+            <div class="col">
+                <ul>聯絡我們
+                    <li>營業時間：周一至周五 09:30 - 18:00</li>
+                    <li>客服電話：02-1234-6789</li>
+                    <li>E-mail：service@gmail.com</li>
+                </ul>
+            </div>
+            <div class="col">
+                <i class="bi bi-facebook fs-1 mx-2 text-white"></i>
+                <i class="bi bi-instagram fs-1 mx-2 text-white"></i>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<div class="d-flex justify-content-center mt-1">
+    <h5>本專題僅為學習用途・不做任何商業使用・圖片來源請見GitHub Repo</h5>
+</div>
+
 </template>
 
 <script>
-
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle'
+// register Swiper custom elements
+register()
 export default {
+
 }
+
 </script>
 
 <style scoped>
+
 </style>

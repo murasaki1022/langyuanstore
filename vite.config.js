2cc54893516a20cdb6324f541ab947bad39fee0c
murasaki1022 <murasaki1022@gmail.com>
2024-03-04 21:51:30 +0800

    修正_plugin-vue_export-helper問題

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import eslintPlugin from 'vite-plugin-eslint'
+
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +24,22 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName (name) {
+          const match = DRIVE_LETTER_REGEX.exec(name)
+          const driveLetter = match ? match[0] : ''
+          // substr 是被淘汰語法，因此要改 slice
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+          )
+        }
+      }
     }
   }
 })
